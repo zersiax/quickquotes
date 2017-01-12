675b1f0f12e977644abcc5fbfdfe142fdbdd5805
Zersiax <florianbeijers@gmail.com>
2017-01-12 17:50:25 +0000

    Moved the form into a modal

--- app/controllers/quotes_controller.rb
+++ app/controllers/quotes_controller.rb
@@ -2,9 +2,6 @@ class QuotesController < ApplicationController
   def index
   @quote = Quote.order("RANDOM()").first
  end 
-def new
-@quote = Quote.new
-end
 
 def create
 @quote = Quote.create(quote_params)
